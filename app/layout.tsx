278250c9cff86436111a70a9f178b6d6aebe0adb
Gerald Sy <markgerald.sy@dict.gov.ph>
2023-08-01 13:27:43 +0800

    first push

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import "@/styles/globals.css"
 import { Metadata } from "next"
 
 import { siteConfig } from "@/config/site"
-import { fontSans } from "@/lib/fonts"
-import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -34,19 +32,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <>
       <html lang="en" suppressHydrationWarning>
         <head />
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
-              <div className="flex-1">{children}</div>
-            </div>
-            <TailwindIndicator />
-          </ThemeProvider>
+        <body className="min-h-screen antialiased bg-background">
+          {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem> */}
+          <div className="relative flex flex-col min-h-screen">
+            <SiteHeader />
+            <div className="flex-1">{children}</div>
+          </div>
+          <TailwindIndicator />
+          {/* </ThemeProvider> */}
         </body>
       </html>
     </>
